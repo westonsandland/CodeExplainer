58c854e32fd992463f1911beeda03a4791af6787
Weston Sandland <westonsandland@gmail.com>
2025-02-01 23:52:01 -0700

    Add in constants compiled files

---
+++ out/constants.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GENAI_BASE_URL = void 0;
+exports.GENAI_BASE_URL = "https://genai-proxy.westonsandland.workers.dev";
+//# sourceMappingURL=constants.js.map
